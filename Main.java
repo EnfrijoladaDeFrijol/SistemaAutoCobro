5b7124d0e2101ca9551f8aeb53342f4622c63b26
Arturo Meza <arturomezsanchez@gmail.com>
2023-06-04 10:37:56 -0600

    [FEATURE] Se sigue completando la opción de 'Agregar'

--- Main.java
+++ Main.java
@@ -6,32 +6,39 @@ import java.util.List;
 public class Main{
 	public static void main(String[] args){
 		Scanner scanOpc = new Scanner(System.in);
+		Scanner scanOpcAgregar = new Scanner(System.in);
+
 		int opc = 0; // Definimos un opción default
 		List<Articulo> misArticulos = new ArrayList<>(); // "carrito de compras"
 		// Leemos el CSV
-		LectorCSV contenidoCSV = new LectorCSV();		
-		contenidoCSV.leerArchivoCSV(".//ProductosBD.csv");
+		LectorCSV contenidoCSV = new LectorCSV(); //Para leer la base datos		
+		contenidoCSV.leerCSV(".//ProductosBD.csv"); // Leemos base datos
 
 		do{
 			imprimirTitulo();
 			imprimirLInea();
 			imprimirMenu();
 
-			opc = elegirOpcion(scanOpc); // Leemos la opción ingresada	
+			opc = elegirOpcion(scanOpc); // Leemos la opción ingresada tipo scanner
 			switch(opc){
 				case 1: // Agregar articulos
+					int opcAgregar = 1;
+					contenidoCSV.mostrarDatos(); // Mostramos el catálogo de productos
 					imprimrMenuAgregar();
-					contenidoCSV.imprimirLinea();
-
-					misArticulos.add(new Articulo("Jamonmm", 34.9, 1001));
-					
+					//contenidoCSV.mostrarUnDato();
+					do{
+						opcAgregar = elegirOpcionAgregar(scanOpcAgregar);
+					}while(opcAgregar != 0);
+					//misArticulos.add(new Articulo("Jamon", 34.9, 1001));
+					break;
+				case 2: // Listar carrito
+					System.out.println("L I S T A M O S   C A R R I T O");
 					listarCarrito(misArticulos);
-					// Hay que agreagar un while
 					break;
-				case 2:
-					System.out.println("HOla 2");
+				case 3: // Pagar
+					System.out.println("P A G A R");
 					break;
-				case 3:
+				case 4:
 					imprimirDespedida();
 					break;
 				default:
@@ -41,7 +48,7 @@ public class Main{
 
 			}// Cierra el switch	
 			
-		}while(opc != 3);
+		}while(opc != 4);
 
 		scanOpc.close();
 	} // Cierra void main
@@ -64,8 +71,9 @@ public class Main{
 		System.out.println("\n\t     --- ( M E N U ) ---");
 		System.out.println("   Ingresa el numero de la opcion que deseas\n");
 		System.out.println("\t\t1) Agregar articulos");
-		System.out.println("\t\t2) Pagar");
-		System.out.println("\t\t3) Salir");
+		System.out.println("\t\t2) Ver carrito"); 
+		System.out.println("\t\t3) Pagar");
+		System.out.println("\t\t4) Salir");
 	}
 	public static int elegirOpcion(Scanner scanOpc){
 		System.out.print("\n\t\t>> ");
@@ -82,10 +90,16 @@ public class Main{
 		// Cada vez que se agregue un ariculo lo creamos
 		System.out.println("\n     --- ( A G R E G A R   A R T I C U L O S) ---");
 		System.out.println("\n\t - Ingresa la clave del articulo");
-		System.out.println(" - Ingresa '0' para salir de este menú");
+		System.out.println("\t - Ingresa '0' para salir de este menu\n");
+	}
+
+	public static int elegirOpcionAgregar(Scanner scanOpcAgregar){
+		System.out.print("  Codigo >> ");
+		int opcion = scanOpcAgregar.nextInt();	
+		return opcion;
+	}
 		
 		// Pedimos datos
-	}
 
 	public static void listarCarrito(List<Articulo> misArticulos){
 		for (Articulo articulo: misArticulos){
